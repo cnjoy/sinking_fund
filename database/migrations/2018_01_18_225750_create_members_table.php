6405dcd1771c6b10b3bd405f5ec998649a6ea1be
joy <cieliton@codev.com>
2018-01-19 08:46:07 +0800

    Initial set up

---
+++ database/migrations/2018_01_18_225750_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('avatar');
+            $table->boolean('is_active')->default(1);
+            $table->decimal('amount');
+            $table->integer('shares');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
