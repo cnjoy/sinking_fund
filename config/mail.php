80924a07fa018ef634d2500d540b83a5ca02edfb
joy <cieliton@codev.com>
2018-06-07 09:33:38 +0800

    Update some config for reset pass

--- config/mail.php
+++ config/mail.php
@@ -57,8 +57,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'codev@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Sinking Fund'),
     ],
 
     /*
